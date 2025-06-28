f3a789d29b46c8111db9b8b2f9eccf7880c58785
Elena_Syurtukova <evasineva@bk.ru>
2025-06-29 02:32:39 +0900

    admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,13 +3,13 @@ from catalog.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name_pr", "purchase_price", "category")
+    list_display = ("id", "name", "price", "category")
     list_filter = ("category",)
-    search_fields = ("name_pr", "description_pr")
+    search_fields = ("name", "description")
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "name_category")
-    search_fields = ("name_category", "description_category")
+    list_display = ("id", "name")
+    search_fields = ("name", "description")
 
 
--- catalog/models.py
+++ catalog/models.py
@@ -2,31 +2,31 @@ from django.db import models
 
 
 class Category(models.Model):
-    name_category = models.CharField(max_length=100, verbose_name='наименование категории')
-    description_category = models.TextField(blank=True, null=True, verbose_name='описание категории')
+    name = models.CharField(max_length=100, verbose_name='наименование категории')
+    description = models.TextField(blank=True, null=True, verbose_name='описание категории')
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
-        ordering = ['name_category']
+        ordering = ['name']
 
     def __str__(self):
-        return self.name_сategory
+        return self.name
 
 
 class Product(models.Model):
-    name_pr = models.CharField(max_length=100, verbose_name='наименование продукта')
-    description_pr = models.TextField(blank=True, null=True, verbose_name='описание продукта')
-    image_pr = models.ImageField(upload_to='catalog.image_pr', blank=True, null=True, verbose_name='вид продукта')
+    name = models.CharField(max_length=100, verbose_name='наименование продукта')
+    description = models.TextField(blank=True, null=True, verbose_name='описание продукта')
+    image = models.ImageField(upload_to='catalog.image_pr', blank=True, null=True, verbose_name='вид продукта')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    purchase_price = models.FloatField(verbose_name='цена покупки')
+    price = models.FloatField(verbose_name='цена покупки')
     created_at = models.DateField(verbose_name='дата создания')
     updated_at = models.DateField(verbose_name='дата последнего изменения')
 
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
-        ordering = ['name_pr', 'category', 'created_at']
+        ordering = ['name', 'category', 'created_at']
 
     def __str__(self):
-        return f'Продукт {self.name_pr} категории {self.category} был создан {self.created_at}'
+        return f'Продукт {self.name} категории {self.category} был создан {self.created_at}'
