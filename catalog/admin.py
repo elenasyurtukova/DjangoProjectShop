f2df933152466469ed008511a578090aab3750cf
Elena_Syurtukova <evasineva@bk.ru>
2025-07-06 14:11:59 +0900

    base, products_list

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,7 +3,7 @@ from catalog.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price", "category")
+    list_display = ("id", "name", "price", "category", "description")
     list_filter = ("category",)
     search_fields = ("name", "description")
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from catalog.models import Product
+
 
 def home(request):
     return render(request, 'home.html')
@@ -11,3 +13,9 @@ def contacts(request):
         name = request.POST.get('name')
         return HttpResponse(f'Спасибо, {name}! Ваше сообщение получено.')
     return render(request, 'contacts.html')
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'products_list.html', context)
