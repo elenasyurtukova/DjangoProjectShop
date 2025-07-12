6a8bc5a53ca715492daab55cac732820501b6089
Elena_Syurtukova <evasineva@bk.ru>
2025-07-12 14:46:31 +0900

    list, detail

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, products_list, product_detail
+from catalog.views import contacts, ProductListView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('contacts/', contacts, name='contacts'),
-    path('', products_list, name='products_list'),
-    path('products/<int:pk>/', product_detail, name='product_detail')
+    path('', ProductListView.as_view(), name='products_list'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from django.views.generic import ListView, DetailView
 from catalog.models import Product
 
 
@@ -15,14 +15,10 @@ def contacts(request):
     return render(request, 'contacts.html')
 
 
-def products_list(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, 'products_list.html', context)
+class ProductListView(ListView):
+    model = Product
 
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, 'product_detail.html', context)
+class ProductDetailView(DetailView):
+    model = Product
 
