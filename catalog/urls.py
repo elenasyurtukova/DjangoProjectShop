cebc04f1d45a8155398b2385101c88b4ad84078a
Elena_Syurtukova <evasineva@bk.ru>
2025-07-12 16:49:34 +0900

    create product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, ProductListView, ProductDetailView
+from catalog.views import contacts, ProductListView, ProductDetailView, ProductCreateView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('contacts/', contacts, name='contacts'),
     path('', ProductListView.as_view(), name='products_list'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail')
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('products/create', ProductCreateView.as_view(), name='product_create')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 from catalog.models import Product
 
 
@@ -21,4 +22,10 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
     model = Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['name', 'description', 'image', 'category', 'price']
+    success_url = reverse_lazy('catalog:products_list')
 
