04a26eb316f7fda9c00e37b713750be93cdfbf37
Elena_Syurtukova <evasineva@bk.ru>
2025-07-19 20:11:16 +0900

    clean_price, clean_image

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,3 +1,5 @@
+import os
+
 from django.forms import ModelForm
 from .models import Product
 from django.core.exceptions import ValidationError
@@ -13,10 +15,9 @@ class ProductForm(ModelForm):
                                                  'placeholder': 'Введите наименование'})
         self.fields['description'].widget.attrs.update({'class': 'form-control',
                                                  'placeholder': 'Добавьте описание'})
-        self.fields['image'].widget.attrs.update({'class': 'form-control',
-                                                        'placeholder': 'Загрузите изображение'})
+        self.fields['image'].widget.attrs.update({'class': 'form-control', })
         self.fields['category'].widget.attrs.update({'class': 'form-control',})
-        self.fields['image'].widget.attrs.update({'class': 'form-control',})
+        self.fields['price'].widget.attrs.update({'class': 'form-control',})
 
 
     def clean(self):
@@ -36,3 +37,13 @@ class ProductForm(ModelForm):
         if price < 0:
             raise ValidationError('Цена покупки не может быть отрицательной')
         return price
+
+    def clean_image(self):
+        image = self.cleaned_data.get('image')
+        if image:
+            if image.size > 5 * 1024 * 1024:
+                raise ValidationError("Размер файла не должен превышать 5MB")
+            extension = os.path.splitext(image.name)[1].lower()
+            if extension not in ['.jpeg', '.jpg', 'png']:
+                raise ValidationError("Файл должен быть в формате jpeg, jpg или png")
+        return image
