1149657a820a367154e7c578a2135a2277a730b0
Elena_Syurtukova <evasineva@bk.ru>
2025-07-24 22:10:03 +0900

    model for users, migrations

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'catalog',
     'blog',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -98,3 +99,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
